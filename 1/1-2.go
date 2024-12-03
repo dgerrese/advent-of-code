fcbd6ad39ba8590854e625ab030fc8d9d7960d22
Dion Gerrese <dgerrese@proton.me>
2024-12-03 11:32:58 +0100

    Added solution for day 1 part 2

---
+++ 1/1-2.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("./input")
+	left := make([]int, 0)
+	right := make([]int, 0)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		numbers := strings.Split(line, "   ")
+		leftNumber, convErr := strconv.Atoi(numbers[0])
+		if convErr != nil {
+			log.Fatal(err)
+		}
+		rightNumber, convErr := strconv.Atoi(numbers[1])
+		if convErr != nil {
+			log.Fatal(err)
+		}
+
+		left = append(left, leftNumber)
+		right = append(right, rightNumber)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	similarity := 0
+	for i := 0; i < len(left); i++ {
+		number := left[i]
+		occurences := 0
+		for j := 0; j < len(right); j++ {
+			if number == right[j] {
+				occurences++
+			}
+		}
+		similarity += number * occurences
+	}
+
+	fmt.Printf("Similarity score: %d\n", similarity)
+}
