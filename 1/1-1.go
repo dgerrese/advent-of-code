387c7e3807ca47095b19c043193d326e18dfd47b
Dion Gerrese <dgerrese@proton.me>
2024-12-03 11:32:44 +0100

    Added solution for day 1 part 1

---
+++ 1/1-1.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("./input")
+	left := make([]int, 0)
+	right := make([]int, 0)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		numbers := strings.Split(line, "   ")
+		leftNumber, convErr := strconv.Atoi(numbers[0])
+		if convErr != nil {
+			log.Fatal(err)
+		}
+		rightNumber, convErr := strconv.Atoi(numbers[1])
+		if convErr != nil {
+			log.Fatal(err)
+		}
+
+		left = append(left, leftNumber)
+		right = append(right, rightNumber)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	slices.Sort(left)
+	slices.Sort(right)
+
+	diff := 0
+	for i := 0; i < len(left); i++ {
+		diff += int(math.Abs(float64(left[i]) - float64(right[i])))
+	}
+
+	fmt.Printf("Total difference: %d\n", diff)
+}
